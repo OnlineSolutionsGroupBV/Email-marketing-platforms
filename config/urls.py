743a0d0e3557f527fe50d9c3103cd9ece0b7ff0f
root <sergej@dergatsjev.be>
2026-08-20 10:38:06 +0000

    Make webmailer root redirect server independent

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from webmailer.views import send_email_api
 from maillogger.views import email_log_api
 
 urlpatterns = [
-    url(r'^$', lambda request: HttpResponseRedirect('http://www.auto-tweedehands.com')),
+    url(r'^$', lambda request: HttpResponseRedirect('/admin/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/send-email/$', send_email_api),
     url(r'^api/email-log/$', email_log_api),
